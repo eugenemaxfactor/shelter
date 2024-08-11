baf7e092890b92664ab2a0c636c2295cd3e4cdc2
eugenemaxfactor <eugene.pivovarius@gmail.com>
2024-08-11 20:03:14 +0400

    shelder2

---
+++ skript.js
@@ -0,0 +1,18 @@
+//console.log('35' + - "22");
+
+//console.log('35' * "22");
+
+//console.log('558' > 22++);
+
+/*let usersCounter = 0;
+let newUsers = usersCounter++;
+console.log(newUsers);*/
+
+//console.log (!false && 11 || 18 && !'');
+
+/*let name = 0;
+console.log(name ?? "No name");
+*/
+
+const BLOCK_HEIGHT = 14 + 50;
+console.log
